9208309445fbda6bc153c0b587c9e7e904d410d1
Aidan O'Grady <aidan@aidanogrady.com>
2015-10-28 20:01:56 +0000

    Created new project called slicer, and now attempting to do that instead.

---
+++ slicer/src/main/java/com/aidanogrady/slicer/Driver.java
@@ -0,0 +1,40 @@
+package com.aidanogrady.slicer;
+
+import com.aidanogrady.slicer.view.Output;
+
+/**
+ * This is responsible for holding the main method, creating the objects that
+ * kick off the control flow.
+ *
+ * @author Aidan O'Grady
+ * @since 0.0
+ */
+public class Driver {
+
+    private static final String ACRONYM = "SLICER";
+
+    private static final String VERSION = "v0.0";
+
+    private static final String NAME =
+            "Slicing Lines of Interesting Code Extremely Roughly";
+
+    private static final String AUTHOR = "Aidan O'Grady - 201218150";
+
+    /**
+     * The main method which sets off control flow.
+     *
+     * @param args - parameters of the program.
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            Output.print("USAGE: SLICER.jar <directory>");
+            System.exit(0);
+        }
+
+        // Print out basic program info.
+        Output.print(ACRONYM + " " + VERSION);
+        Output.print(NAME);
+        Output.print(AUTHOR);
+        Output.lineBreak();
+    }
+}
